1d5f9dcd8c29b22ae825d2568d244f7a4bcace62
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2023-06-19 20:42:44 +0300

    Commit 2

---
+++ Rummage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCode
+{
+    internal class Rummage
+    {
+        public static int Search(int[] array, int element)
+        {
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] == element)
+                    return i;
+
+            return -1;
+        }
+    }
+}
