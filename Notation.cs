ff94cce4e47f4458d8765ab9259cd3a741fe50b3
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2023-07-04 16:25:46 +0300

    Commit notation

---
+++ Notation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCode
+{
+    class Notation
+    {
+        int armySize = 10;
+        int number = 10;
+        string name = "Vladislav";
+    }
+}
