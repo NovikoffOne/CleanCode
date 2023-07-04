feac613bf36c0c52f3150b0c8718c667060b3bcc
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2023-07-04 16:20:24 +0300

    Commit magicNumber

---
+++ MagicNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCode
+{
+    class Weapon
+    {
+        private int _bullets;
+        private int _bulletPerShot;
+        private int _noBullet;
+
+        public bool CanShoot() => _bullets > _noBullet;
+
+        public void Shoot() => _bullets -= _bulletPerShot;
+    }
+}
