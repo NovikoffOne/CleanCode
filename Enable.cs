5a79989f6974eb2c4b59b62c07cff73eaa06bc3b
NovikoffOne <104835127+NovikoffOne@users.noreply.github.com>
2023-07-04 17:17:36 +0300

    Create Enable.cs

---
+++ Enable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCode
+{
+    class Enable
+    {
+        public void Enable(bool enable)
+        {
+            if (enable == true)
+                _effects.StartEnableAnimation();
+            else
+                Disable();
+        }
+
+        public void Disable()
+        {
+            _pool.Free(this);
+        }
+    }
+}
